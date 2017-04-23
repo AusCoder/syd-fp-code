1889ba36c5032fb2d98fbf75db0d843f272be3bf
Sebastian Mueller <sebmuellermath@gmail.com>
2017-04-23 22:13:47 +1000

    added concurrent hash map, updated slides

--- src/main/scala/com/sebmuellermath/Boot.scala
+++ src/main/scala/com/sebmuellermath/Boot.scala
@@ -1,22 +1,40 @@
 package com.sebmuellermath
 
+import com.sebmuellermath.checks.Check
 import com.sebmuellermath.service.{CheckService, RequestDispatcher, WebhookService}
 import com.sebmuellermath.domain.{DispatchedRequest, JobId, Request}
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.server.{Server, ServerApp}
-
 import scalaz._
 import Scalaz._
 import scalaz.concurrent.Task
+import fs2._
+import fs2.time.awakeEvery
+import scala.concurrent.duration._
+import fs2.interop.scalaz._
 
 
 object Boot extends ServerApp {
   val webhookUrl = "http://localhost:37047/results"
   val sampleRequest = Request(JobId("1"), 50, webhookUrl)
+  implicit val S = Scheduler.fromFixedDaemonPool(5) // required by awakeEvery
 
-  val checkService = CheckService()
+  val checks = Check.checkWithPreloadedResults
+  val checkService = CheckService(checks)
   val dispatcher = RequestDispatcher.getSimpleDispatcher("http://localhost:5000", checkService)
   val webhookService = WebhookService(checkService)
+
+  val requestStream: Stream[Task, DispatchedRequest] = {
+    awakeEvery(1.second)
+      .zip(Stream.range(0, Int.MaxValue))
+      .map(_._2)
+      .map(_.toString)
+      .map(JobId.apply)
+      .map(id => Request(id, 50, webhookUrl))
+      .flatMap(req => Stream.eval(dispatcher.dispatchAndSubmit(req)))
+  }
+
+  requestStream.run.unsafePerformAsync(_ => ())
 
   val task = dispatcher.dispatchAndSubmit(sampleRequest)
   val many = for {
@@ -24,17 +42,8 @@ object Boot extends ServerApp {
     _ <- task
     _ <- task
     _ <- task
-    _ <- task
-    _ <- task
-    _ <- task
-    _ <- task
-    _ <- task
-    _ <- task
-    _ <- task
-    _ <- task
-    _ <- task
   } yield ()
-  many.unsafePerformAsync(_ => ())
+//  many.unsafePerformAsync(_ => ())
 
   def server(args: List[String]): Task[Server] = {
     BlazeBuilder
--- src/main/scala/com/sebmuellermath/service/CheckService.scala
+++ src/main/scala/com/sebmuellermath/service/CheckService.scala
@@ -1,5 +1,6 @@
 package com.sebmuellermath.service
 
+import java.util.concurrent.ConcurrentHashMap
 import com.sebmuellermath.domain._
 import com.sebmuellermath.checks.Check
 import scalaz._
@@ -11,21 +12,29 @@ import scalaz.concurrent.Task
   */
 case class CheckService(check: Check) {
 
-  var store: Map[JobId, Request] = Map.empty // make this concurrent hashmap
+  val store: ConcurrentHashMap[JobId, Request] = new ConcurrentHashMap[JobId, Request]()
+
+  def getFromStore(id: JobId): Option[Request] = {
+    Option(store.get(id))
+  }
 
   def submitDispatchedRequest(dispatchedRequest: DispatchedRequest): Task[Unit] = Task {
     dispatchedRequest.fold(
       e => println(s"error dispatching request: $e"),
-      req => store = store + (req.id -> req)
+      req => {
+        store.put(req.id, req)
+        ()
+      }
     )
   }
 
-  def liftedRunCheck(request: Request, response: Response): Task[CheckResult] = Task {
-    check.runCheck(request, response)
-  }
+  def liftedRunCheck(request: Request, response: Response): Task[CheckResult] =
+    Task {
+      check.runCheck(request, response)
+    }
 
   def submitResponse(response: Response): Task[Unit] = {
-    store.get(response.id).fold(
+    getFromStore(response.id).fold(
       Task(println("response with no matching request")))(
       request => liftedRunCheck(request, response).flatMap(reportResults)
     )
